503040378ca49b2190148f3c320ce1ddab0e744d
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2023-09-12 00:15:57 +0530

    tailwind css added to project

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen w-[110px] flex flex-col items-center justify-start gap-5">
+      <h2>OctaLogic</h2>
+      <h2>Home</h2>
+      <h2>Courses</h2>
+    </div>
+  );
+};
+
+export default Sidebar;
