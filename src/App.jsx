2f9cb59f8c16fe3966306f64441e1aa93145224c
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2023-09-12 13:31:16 +0530

    side bar almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import MainRoute from "./routes/MainRoute";
 
 function App() {
   return (
-    <div>
+    <div className="flex">
       <Sidebar />
+      <div className="w-[calc(100%-110px)] bg-slate-600">
+        <MainRoute />
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-
+import logo from "../assets/logo.png";
+import dashboard from "../assets/dashboard.svg";
+import icmusic from "../assets/icmusic.svg";
 const Sidebar = () => {
   return (
-    <div className="h-screen w-[110px] flex flex-col items-center justify-start gap-5">
-      <h2>OctaLogic</h2>
-      <h2>Home</h2>
-      <h2>Courses</h2>
+    <div className="h-[1024px] w-[96px] flex flex-col items-center justify-start gap-[48px] px-[12px] py-[10px] shrink-0">
+      <img
+        className="w-[48px] h-[48px] shrink-0 cursor-pointer"
+        src={logo}
+        alt="Octalogic Logo"
+      />
+      <div className="h-[58px] w-[58px] flex flex-col items-center justify-center gap-[1px] bg-[#FEDFE1] cursor-pointer">
+        <img src={dashboard} alt="Dashboard" />
+        <p className="text-xs text-[#901E75] font-normal">Home</p>
+      </div>
+      <div className="h-[58px] w-[58px] flex flex-col items-center justify-center gap-[1px] bg-[#E5E7EB] cursor-pointer">
+        <img src={icmusic} alt="Courses" />
+        <p className="text-xs text-[#83858B] font-normal">Courses</p>
+      </div>
     </div>
   );
 };
